3807ec3ef50f95b7f7b8a7611320d7a90100be1a
Ajaree Chawengwongwan <Ajaree Chawengwongwan>
2016-04-06 13:39:21 +0700

    intial CarFuel Project

---
+++ CarFuel.DataAccess/CarFuelDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarFuel.Models;
+using System.Data.Entity;
+
+namespace CarFuel.DataAccess
+{
+    public class CarFuelDb : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+
+}
